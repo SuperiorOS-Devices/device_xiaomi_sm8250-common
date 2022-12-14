783e38066a0d8076fd2fe5e2d293f9256622448c
EmanuelCN <emanuelgithub@gmail.com>
2022-12-14 23:29:58 +0000

    sm8250-common: Rename libperfmgr to xiaomi-sm8250-libperfmgr

--- kona.mk
+++ kona.mk
@@ -434,7 +434,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-sm8250-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
