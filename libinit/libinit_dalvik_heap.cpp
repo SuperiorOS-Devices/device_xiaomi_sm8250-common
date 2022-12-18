ff8781257d633bd9f9e337cadbae5225a43c776d
EmanuelCN <emanuelgithub@gmail.com>
2022-12-18 16:53:48 +0000

    sm8250-common: Tune dalvik values

--- libinit/libinit_dalvik_heap.cpp
+++ libinit/libinit_dalvik_heap.cpp
@@ -22,9 +22,9 @@ static const dalvik_heap_info_t dalvik_heap_info_6144 = {
     .heapstartsize = "16m",
     .heapgrowthlimit = "256m",
     .heapsize = "512m",
-    .heapminfree = "8m",
-    .heapmaxfree = "32m",
-    .heaptargetutilization = "0.5",
+    .heapminfree = "512k",
+    .heapmaxfree = "8m",
+    .heaptargetutilization = "0.75",
 };
 
 static const dalvik_heap_info_t dalvik_heap_info_4096 = {
