ed40afa379d2dbafc47cdfe7e369c46a18a1a5b0
johnmart19 <johnivan19999@gmail.com>
2023-02-19 01:15:43 +0200

    sm8250-common: bluetooth: Compile missing lib for ldac
    I a2dp_vendor_ldac_decoder: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:135 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    E a2dp_vendor_ldac: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac.cc:1418 init: init: cannot load the decoder

--- kona.mk
+++ kona.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Bluetooth LDAC
+PRODUCT_PACKAGES += \
+    libldacBT_bco
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
